ee9b89f8222bef68430a1462d9688305a9d17ff3
Htet Oo Wai Yan <htetoo.waiyan@outlook.com>
2023-06-29 14:32:19 +0630

    feat: add searchbox and select ui

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import Link from "next/link";
+import Toolbar from "#/ui/toolbar";
 import { Poster, Posters } from "#/ui/posters";
 import type { Movie } from "#/app/api/movies/movie";
 import { getMovies } from "#/app/api/movies/getMovies";
@@ -21,10 +22,13 @@ export default async function HomePage() {
   const movies = await getMovies();
 
   return (
-    <Posters>
-      {movies.map((movie) => {
-        return <PosterItem key={movie.id} movie={movie} />;
-      })}
-    </Posters>
+    <div className="w-full">
+      <Toolbar />
+      <Posters>
+        {movies.map((movie) => {
+          return <PosterItem key={movie.id} movie={movie} />;
+        })}
+      </Posters>
+    </div>
   );
 }
