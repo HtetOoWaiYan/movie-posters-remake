1eb6452229bd0fe9e258bdf87e6b443b4a599e4c
Htet Oo Wai Yan <htetoo.waiyan@outlook.com>
2023-06-29 14:30:37 +0630

    feat: update loading indicator

--- app/loading.tsx
+++ app/loading.tsx
@@ -1,7 +1,10 @@
 export default function Loading() {
   return (
-    <span className="mx-auto animate-pulse text-center duration-75">
-      Loading ...
+    <span className="absolute left-2/4 top-1/3 animate-pulse text-center duration-75">
+      <span
+        className="loading loading-ring loading-lg"
+        aria-label="loading"
+      ></span>
     </span>
   );
 }
